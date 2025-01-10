e9bf1f564e470d3b7c2a8150b685fe108fddd18b
Anna <anna.kvernplassen@gmail.com>
2025-01-10 14:59:44 +0100

    add images

--- src/levels.ts
+++ src/levels.ts
@@ -1,13 +1,13 @@
-const player1: number = 1;
-const player2: number = 2;
-const transporter: number = 3;
-const platform: number = 4;
-const trampoline: number = 5;
-const snowman: number = 6;
-const timerPlayer1: number = 8;
-const timerPlayer2: number = 9;
-const frameX: number = 144;
-const frameY: number = 102.4;
+// const player1: number = 1;
+// const player2: number = 2;
+// const transporter: number = 3;
+// const platform: number = 4;
+// const trampoline: number = 5;
+// const snowman: number = 6;
+// const timerPlayer1: number = 8;
+// const timerPlayer2: number = 9;
+// const frameX: number = 144;
+// const frameY: number = 102.4;
 
 const level1: number[][] = [
   [8, 0, 0, 0, 0, 0, 0, 0, 0, 9],
@@ -20,32 +20,34 @@ const level1: number[][] = [
   [0, 0, 0, 0, 0, 0, 0, 0, 5, 0],
 ];
 
+const gameObjects = [];
+
 for (let y = 0; y <= level1.length; y++) {
   for (let x = 0; x < level1[y].length; y++) {
     let value = level1[y][x];
     if (value === 1) {
-      const player1 = new Player();
+      gameObjects.push(new Player("yellow", x, y));
     }
     if (value === 2) {
-      const player2 = new Player();
+      gameObjects.push(new Player("green", x, y));
     }
     if (value === 3) {
-      const transporter = new Transporter();
+      gameObjects.push(new Transporter());
     }
     if (value === 4) {
-      const platform = new Platform();
+      gameObjects.push(new Platform());
     }
     if (value === 5) {
-      const trampoline = new Trampoline();
+      gameObjects.push(new Trampoline());
     }
     if (value === 6) {
-      const snowman = new Snowman();
+      gameObjects.push(new Snowman());
     }
     if (value === 8) {
-      const timerPlayer1 = new Timer();
+      gameObjects.push(new Timer("yellow"));
     }
     if (value === 9) {
-      const timerPlayer2 = new Timer();
+      gameObjects.push(new Timer("green"));
     }
   }
 }
