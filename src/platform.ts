2188e9db8ba644361500a9a97a509e5d54d77c78
Anna <anna.kvernplassen@gmail.com>
2025-01-14 22:27:52 +0100

    structure of small GameObject classes (snowman, trampoline etc)

---
+++ src/platform.ts
@@ -0,0 +1,8 @@
+class Platform extends GameObject {
+  constructor(position: p5.Vector) {
+    super(146, 30, "/assets/images/platform.svg", true, position);
+  }
+  public draw() {}
+
+  public update() {}
+}
---
+++ src/trampoline.ts
@@ -0,0 +1,8 @@
+class Trampoline extends GameObject {
+  constructor(position: p5.Vector) {
+    super(83, 108, "/assets/images/trampoline.svg", true, position);
+  }
+  public draw() {}
+
+  public update() {}
+}
