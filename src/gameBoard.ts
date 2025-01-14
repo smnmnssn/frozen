a8b3cb1d59fafc4774cc5be9775b3d26083a3f13
Anna <anna.kvernplassen@gmail.com>
2025-01-14 19:52:37 +0100

    preparing and structuring classes

---
+++ src/gameBoard.ts
@@ -0,0 +1,25 @@
+class GameBoard {
+  private gameObjects: GameObjects[];
+
+  constructor() {
+    this;
+  }
+
+  private checkCollisions() {}
+
+  private bouncePlayers() {}
+
+  private squishToGround() {}
+
+  private freezeToIcicle() {}
+
+  private teleportPlayer() {}
+
+  private applyNoFriction() {}
+
+  private switchChaser() {}
+
+  private checkWinner() {}
+
+  private checkTimer() {}
+}
